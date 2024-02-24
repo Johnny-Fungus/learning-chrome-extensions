c708e649edbcbfe8a9731425aa275b6353567461
Johnny-Fungus <150959957+Johnny-Fungus@users.noreply.github.com>
2024-02-24 01:56:52 -0600

    finished extension

---
+++ scripts/content.js
@@ -0,0 +1,3 @@
+const image = document.createElement("img");
+image.src = "https://cdn1.epicgames.com/ue/product/Screenshot/MushroomMan0-1920x1080-c5770dd2bc9643ace72c75141ba9908b.jpg?resize=1&w=1600"
+document.body.appendChild(image);
